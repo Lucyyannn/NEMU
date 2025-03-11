2a2acf304cd47ff34b038e1bcc389bd9150a7260
tracer-ics2017 <tracer@njuics.org>
2025-03-11 18:54:09 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:54:09 up 35 min,  1 user,  load average: 0.41, 0.27, 0.20 be3806ad59e7eb84413639f6f670e7002edc1c5

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -46,11 +46,11 @@ void reg_test() {
 void print_reg_info(){
   for(int i=0;i<8;i++){
     printf("| %s  ",reg_name(i,4));
-    printf("%X     ",reg_l(i));
+    printf("%X08     ",reg_l(i));
     printf("| %s  ",reg_name(i,2));
-    printf("%X     ",reg_w(i));
+    printf("%X04     ",reg_w(i));
     printf("| %s  ",reg_name(i,1));
-    printf("%X      |\n",reg_b(i));
+    printf("%X02      |\n",reg_b(i));
   }
   printf("pc     %X\n",cpu.eip);
   return ;
