94f31c78cd0489f8b83b5b1d70298fe807d0ccda
tracer-ics2017 <tracer@njuics.org>
2025-03-11 19:47:35 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:47:35 up  1:29,  1 user,  load average: 0.41, 0.31, 0.29 960597d946492bae62b926c2dc90e93923e33d5e

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -155,8 +155,10 @@ static bool make_token(char *e) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-        // Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-        //     i, rules[i].regex, position, substr_len,substr_len,substr_start);
+        printf("substr_start:%s",substr_start);
+
+        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+            i, rules[i].regex, position, substr_len,substr_len,substr_start);
         position += substr_len;
 
         printf("here0 ");
