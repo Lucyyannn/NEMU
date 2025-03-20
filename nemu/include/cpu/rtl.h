53291998e91b5b434ef375906505e490eddd899f
tracer-ics2017 <tracer@njuics.org>
2025-03-20 11:19:33 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:19:33 up  2:04,  1 user,  load average: 0.30, 0.37, 0.35 f2bf2099f989ebf30ad021631918405fe71f07

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -157,13 +157,13 @@ static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
   cpu.esp = cpu.esp-4;
-  vaddr_write(cpu.esp,DATALEN,*src1); // bytes ? *4/8???
+  paddr_write(cpu.esp,DATALEN,*src1); // bytes ? *4/8???
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-  *dest = vaddr_read(cpu.esp,DATALEN);
+  *dest = paddr_read(cpu.esp,DATALEN);
   cpu.esp = cpu.esp+4;
 }
 
