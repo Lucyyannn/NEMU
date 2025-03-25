dc926a148ec1987f76ead2d530bb798687e1e096
tracer-ics2017 <tracer@njuics.org>
2025-03-25 10:35:18 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:35:18 up 52 min,  1 user,  load average: 0.07, 0.22, 0.25 86f23a2a533adf29c734fbe758ef979fef3e60cd

--- nemu/include/cpu/exec.h
+++ nemu/include/cpu/exec.h
@@ -11,10 +11,11 @@ typedef void (*EHelper) (vaddr_t *);
 // fetch a instruction
 static inline uint32_t instr_fetch(vaddr_t *eip, int len) {
   uint32_t instr = vaddr_read(*eip, len);
-  printf("before debug:\n");
+
 #ifdef DEBUG
   uint8_t *p_instr = (void *)&instr;
   int i;
+  printf("now eip is: 0x%08x \n",*eip);
   printf("the instr: \n");
   for (i = 0; i < len; i ++) {
     decoding.p += sprintf(decoding.p, "%02x ", p_instr[i]);
