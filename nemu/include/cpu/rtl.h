841fdc9da6c0295210e071048f22157cebfb6969
2213583-Lu Yihan <2584299050@qq.com>
2025-03-26 16:03:25 +0800

    PA2-2 finish debugging!

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -150,7 +150,6 @@ static inline void rtl_not(rtlreg_t* dest) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   //dest <- signext(src1[(width * 8 - 1) .. 0])
-
   int shift = 32 - width*8;
   *dest= ((int32_t)(*src1 << shift)) >> shift;
 }
