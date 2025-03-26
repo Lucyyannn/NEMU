9e341bcad64e3991885ce59c9910e00dc91368b7
tracer-ics2017 <tracer@njuics.org>
2025-03-26 15:42:39 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:42:39 up  1:45,  1 user,  load average: 1.08, 0.70, 0.42 a539fe1544282c925cd38a349fa37d1ffe34c344

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -151,7 +151,7 @@ static inline void rtl_not(rtlreg_t* dest) {
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   //dest <- signext(src1[(width * 8 - 1) .. 0])
   int sign = (*src1>>(width*8-1))&1;
-  int ab_val=(*src1 << (32-width*8+1)) >> (32-width*8+1);
+  int ab_val=(*src1 << (32-width*8)) >> (32-width*8);
   if(sign){
     *dest = (int)(-ab_val);
   }else{
