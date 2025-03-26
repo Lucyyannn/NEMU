d87a9e3391da2c52b211413fde5198056a8ce6ee
tracer-ics2017 <tracer@njuics.org>
2025-03-26 14:44:11 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  14:44:11 up 46 min,  1 user,  load average: 0.47, 0.29, 0.23 5203e8a8986f47bfe245496e89ecb217cc4357c5

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -213,9 +213,10 @@ make_EHelper(imul1) {
 make_EHelper(imul2) {
   rtl_sext(&id_src->val, &id_src->val, id_src->width);
   rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
-
+  printf("two operands: %d, %d \n",id_src->val, id_dest->val);
   rtl_imul(&t0, &t1, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t1);
+  printf("result: %d, \n",t1);
 
   print_asm_template2(imul);
 }
