e48a356a25010f807a3881aa07776b7c5e0e565d
tracer-ics2017 <tracer@njuics.org>
2025-03-26 13:11:38 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  13:11:38 up  2:57,  1 user,  load average: 0.64, 0.45, 0.37 790c54f6ad8167b1577a4bdf425881e5838f36e9

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -24,11 +24,11 @@ static inline void set_width(int width) {
 /* Instruction Decode and Execute */
 static inline void idex(vaddr_t *eip, opcode_entry *e) {
   /* eip is pointing to the byte next to opcode */
-  printf("in Index, befor decode, next eip = %02x \n",*eip);
+  //printf("in Index, befor decode, next eip = %02x \n",*eip);
   if (e->decode){
     e->decode(eip);
   }
-  printf("in Idex,before execute,next eip is %02x\n",*eip);
+  //printf("in Idex,before execute,next eip is %02x\n",*eip);
   e->execute(eip);
 
 }
