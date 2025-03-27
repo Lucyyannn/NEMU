f8bedc1f1339a76e9c12413eaa9d632c451c523d
tracer-ics2017 <tracer@njuics.org>
2025-03-27 16:22:14 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:22:14 up 37 min,  1 user,  load average: 0.74, 0.59, 0.41 75f56bb9ac5303c27c1f4f0159b30b0170dbfe62

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -77,9 +77,7 @@ make_EHelper(cwtl) {
 
 make_EHelper(movsx) {
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
-  printf("src->val: 0x%04x, src->width: %d \n",id_src->val,id_src->width);
   rtl_sext(&t2, &id_src->val, id_src->width);
-  printf("sext: 0x%08x \n",t2);
 
   operand_write(id_dest, &t2);
   print_asm_template2(movsx);
