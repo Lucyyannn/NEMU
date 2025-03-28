e701644106de92069904f54893f495c9bd593f43
tracer-ics2017 <tracer@njuics.org>
2025-03-28 16:02:03 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:02:03 up 42 min,  1 user,  load average: 0.29, 0.16, 0.10 edf76fe4c0c1ac2dc5f51ec3fd2fe8d7e8b731b

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,6 +3,9 @@
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  void *buffer;
+  size_t len = get_ramdisk_size();
+  ramdisk_read(buffer,0,len);
+  memcpy(DEFAULT_ENTRY,buffer,len);
   return (uintptr_t)DEFAULT_ENTRY;
 }
