c2598a1af04b8111921d6d59edb4e5ee3a31cbf0
tracer-ics2017 <tracer@njuics.org>
2025-03-28 19:47:28 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:47:28 up 2 min,  1 user,  load average: 0.63, 0.48, 0.20 221a309083bb7ee3d953c30f70ee8e81188e2de1

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -9,4 +9,11 @@ typedef char bool;
 #define true 1
 #define false 0
 
+
+//for ramdisk
+void ramdisk_read(void *buf, off_t offset, size_t len);
+void ramdisk_write(const void *buf, off_t offset, size_t len);
+size_t get_ramdisk_size();
+
+
 #endif
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,11 +1,13 @@
 #include "common.h"
+#include <stdlib.h>
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  void *buffer;
   size_t len = get_ramdisk_size();
-  ramdisk_read(buffer,0,len);
-  memcpy(DEFAULT_ENTRY,buffer,len);
+
+  ramdisk_read(DEFAULT_ENTRY,0,len);
+
   return (uintptr_t)DEFAULT_ENTRY;
 }
