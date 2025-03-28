8b7912f0f2b78e2f4ff24c574858368f73a5a1dd
tracer-ics2017 <tracer@njuics.org>
2025-03-28 21:25:17 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:25:17 up  1:40,  1 user,  load average: 0.11, 0.20, 0.17 d6296d94d9c92b9e5873b9e0abdc1bc9bb9758e4

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,7 +7,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-  if(NO*8>cpu.idtr.limit){
+  if(NO>cpu.idtr.limit){
     panic("No in raise_intr is beyond IDT's limit!");
     return ;
   }
@@ -18,12 +18,12 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push(&ret_addr);
   
   //read the base of IDT , index the idt to find the GD
-  uint32_t idt_index = cpu.idtr.base + NO*8;
+  uint32_t idt_index = cpu.idtr.base + NO*8;//8 bytes for each GD
   uint32_t gd_l = vaddr_read(idt_index, HALF_GD);
   uint32_t gd_h = vaddr_read(idt_index+HALF_GD,HALF_GD);
 
   //combine the offset , get the destination
-  uint32_t destination = (gd_l&0xFFFF)|(gd_h&0xFFFF0000);
+  uint32_t destination = (gd_l&0x0000FFFF)|(gd_h&0xFFFF0000);
   //jmp to the destination
   decoding.is_jmp = true;
   decoding.jmp_eip = destination;
