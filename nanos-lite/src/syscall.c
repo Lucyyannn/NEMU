f1c8d77a4259e6750336d60ff12c95661394c132
tracer-ics2017 <tracer@njuics.org>
2025-03-31 19:26:14 +0800

    >  run 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:26:14 up  1:16,  1 user,  load average: 0.37, 0.31, 0.29 50675b58aefdb1ed9b194d9f4a390f331334baa5

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,8 @@
 #include "common.h"
 #include "syscall.h"
 
+ssize_t fs_write(int fd, const void *buf, size_t len);
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -17,11 +19,13 @@ _RegSet* do_syscall(_RegSet *r) {
       size_t count = (size_t)a[3];
 
       if(fd==1||fd==2){
-        for(int i=0;i<count;i++){
-          _putc(*(buf+i));
-        }
+        //r->eax = fs_write(fd, buf,count);
+        _putc(*buf);
+        // for(int i=0;i<count;i++){
+        //   _putc(*(buf+i));
+        // }
       //return value: stdout(1):count; stderr(2):-1
-        if(a[1]==1){
+        if(fd ==1){
           r->eax = count;
         }else{
           r->eax = -1;
