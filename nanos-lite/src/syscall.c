3bf844b319e44135057c7cf29ebbcf964239c991
tracer-ics2017 <tracer@njuics.org>
2025-03-31 19:29:50 +0800

    >  run 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:29:50 up  1:19,  1 user,  load average: 0.83, 0.62, 0.42 1360cf00cd1c7bdbd9d9d7bde064a596fd0baeed

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,31 +11,30 @@ _RegSet* do_syscall(_RegSet *r) {
   a[3] = SYSCALL_ARG4(r);
   switch (a[0]) {
     case 0://SYS_none
-      SYSCALL_ARG1(r) = 1;
+      r->eax = 1;
       break;
     case 3:{//SYS_write
       int fd       = (int)a[1];
-      //char *buf    = (void*)a[2];
+      char *buf    = (void*)a[2];
       size_t count = (size_t)a[3];
 
       if(fd==1||fd==2){
         //r->eax = fs_write(fd, buf,count);
-        //_putc(*buf);
-        // for(int i=0;i<count;i++){
-        //   _putc(*(buf+i));
-        // }
+        for(int i=0;i<count;i++){
+          _putc(*(buf+i));
+        }
       //return value: stdout(1):count; stderr(2):-1
         if(fd ==1){
-          SYSCALL_ARG1(r) = count;
+          r->eax = count;
         }else{
-          SYSCALL_ARG1(r) = -1;
+          r->eax = -1;
         }
       }
       break;
     }
     case 4:{//SYS_exit
       _halt(a[1]); 
-      SYSCALL_ARG1(r) = 1;
+      r->eax = 1;
       break;
     }
     default: 
