823c969169901d76b163673ee57f108f86121b36
tracer-ics2017 <tracer@njuics.org>
2025-03-31 11:06:42 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:06:42 up 55 min,  1 user,  load average: 0.11, 0.27, 0.23 8cc23f7757a804caa0de0f27cb150c70a4814527

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -11,6 +11,8 @@ make_EHelper(lidt) {
     cpu.idtr.base  = vaddr_read(id_dest->val+2,3);
   }else if(id_dest->width==4){
     cpu.idtr.limit = vaddr_read(id_dest->val,2);
+    printf("2~4: %x \n",vaddr_read(id_dest->val+2,2));
+    printf("4~6: %x \n",vaddr_read(id_dest->val+4,2));
     cpu.idtr.base  = vaddr_read(id_dest->val+2,4);
   }
   printf("after:\n");
