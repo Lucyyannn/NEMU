3998d19589e5db1ef2487c3c42e7a7961ed1a0c4
tracer-ics2017 <tracer@njuics.org>
2025-04-01 21:09:34 +0800

    >  run 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:09:34 up  1:39,  1 user,  load average: 0.48, 0.47, 0.37 d198c3fcbb4ec20e4d78c9b78d80ed801d86194f

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -40,7 +40,7 @@ void *_sbrk(intptr_t increment){
   // if(_syscall_(SYS_brk,new_programbreak,(uintptr_t)0,(uintptr_t)0)!=0){
   //   return (void*)-1;
   // }
-  // _end = new_programbreak;
+   programbreak = new_programbreak;
   return (void*)programbreak;
 
 }
