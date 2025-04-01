7931bbe36ef73d46a5a9a1b88e893afacfe66edd
tracer-ics2017 <tracer@njuics.org>
2025-04-01 11:19:47 +0800

    >  run 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:19:47 up  1:56,  1 user,  load average: 0.20, 0.12, 0.10 d61dd57c62850abf654053ec830701f21aa9b462

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -6,13 +6,11 @@
 int fs_open(const char *pathname, int flags, int mode);
 size_t fs_filesz(int fd);
 ssize_t fs_read(int fd, void *buf, size_t len);
-
+int fs_close(int fd);
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  //size_t len = get_ramdisk_size();
-
   int fd = fs_open(filename,0,0);
   fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));
-
+  fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
