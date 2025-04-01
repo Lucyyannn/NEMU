453da6b54702cf8fa473d4760251c8475da29c87
tracer-ics2017 <tracer@njuics.org>
2025-04-01 18:53:04 +0800

    >  run 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:53:04 up  3:15,  1 user,  load average: 0.18, 0.28, 0.33 239ee398c37054ebcdff85579e2c7adaba91e326

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -51,7 +51,7 @@ size_t fs_filesz(int fd){
 }
 
 ssize_t fs_read(int fd, void *buf, size_t len){
-  assert(len>=0 && len<=fs_filesz(fd));
+  //assert(len>=0 && len<=fs_filesz(fd));
   ramdisk_read(buf,file_table[fd].disk_offset,len);
   file_table[fd].open_offset += len;
 
@@ -59,7 +59,7 @@ ssize_t fs_read(int fd, void *buf, size_t len){
 }
 
 ssize_t fs_write(int fd, const void *buf, size_t len){
-  assert(len>=0 && len<=fs_filesz(fd));
+  //assert(len>=0 && len<=fs_filesz(fd));
   ramdisk_write(buf,file_table[fd].disk_offset,len);
   file_table[fd].open_offset += len;
   return len;
