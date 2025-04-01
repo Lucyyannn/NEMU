104aaaee6e0aa20d9afdc80443a6748a26d42a10
tracer-ics2017 <tracer@njuics.org>
2025-04-01 21:12:23 +0800

    >  run 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:12:23 up  1:42,  1 user,  load average: 0.20, 0.41, 0.36 d196a7d9efdbaaa539acf95bc7476c47e70ee09

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -7,7 +7,7 @@
 #include "syscall.h"
 
 extern char _end;
-static uintptr_t end = &_end;
+// static uintptr_t end = &_end;
 
 // TODO: discuss with syscall interface
 #ifndef __ISA_NATIVE__
@@ -50,7 +50,8 @@ int _read(int fd, void *buf, size_t count) {
 }
 
 int _close(int fd) {
-  return _syscall_(SYS_close, (uintptr_t)fd);
+  //return _syscall_(SYS_close, (uintptr_t)fd);
+  return 0;
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
