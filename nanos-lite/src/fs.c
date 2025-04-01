02d62db2facef2cd75affaf83c9fc040e524edf9
tracer-ics2017 <tracer@njuics.org>
2025-04-01 19:02:20 +0800

    >  run 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:02:20 up  3:24,  1 user,  load average: 0.23, 0.42, 0.42 ff3fefc77cf50b17357ccd632a667abf6cceec0c

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -85,4 +85,7 @@ off_t fs_lseek(int fd, off_t offset, int whence){
   return file_table[fd].open_offset;
 }
 
-
+void initSize(int fd,size_t size){
+  file_table[fd].size = size;
+  return;
+}
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -6,11 +6,14 @@ int fs_open(const char *pathname, int flags, int mode);
 size_t fs_filesz(int fd);
 ssize_t fs_read(int fd, void *buf, size_t len);
 int fs_close(int fd);
+void initSize(int fd,size_t size);
 
 uintptr_t loader(_Protect *as, const char *filename) {
   int fd = fs_open(filename,0,0);
   fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));
+  initSize(fd,fs_filesz(fd));
   printf("filesz when load: %d \n",fs_filesz(fd));
+
   fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
