d0df330a1a25c0e35feb035a4fb8e3d914bcfd77
tracer-ics2017 <tracer@njuics.org>
2025-04-01 20:07:58 +0800

    >  run 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:07:58 up 37 min,  1 user,  load average: 0.05, 0.09, 0.11 c2a2ececb65551b082600c91e24ba0467a003d65

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -2,7 +2,6 @@
 #include "memory.h"
 
 static void *pf = NULL;
-extern uintptr_t _end;
 
 void* new_page(void) {
   assert(pf < (void *)_heap.end);
@@ -17,7 +16,6 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uint32_t new_brk) {
-  _end = new_brk;
   return 0;
 }
 
