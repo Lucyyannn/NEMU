ff7c66d867ea9d304231e70ec1921c1073ee71be
tracer-ics2017 <tracer@njuics.org>
2025-04-02 19:02:32 +0800

    >  run 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:02:32 up 35 min,  1 user,  load average: 0.26, 0.25, 0.26 c5e618d77b6e837ffb796d696d1f314da4e6357

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -62,5 +62,4 @@ void init_device() {
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
   sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", _screen.width, _screen.height);
-  void init_i8042();
 }
