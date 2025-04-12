953f4a25eb14c901379976b0fd49080516684899
tracer-ics2017 <tracer@njuics.org>
2025-04-12 12:19:02 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  12:19:02 up  2:23,  1 user,  load average: 0.42, 0.24, 0.20 903abcdb81ae233772fa5da4bcbf5624d3878d0

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -13,18 +13,34 @@
     })
 
 uint8_t pmem[PMEM_SIZE];
-/*
+/*  VADDR:
+
    31----------22 21----------12 11----------0
       DIR(10)         PAGE(10)      OFFSET(12)
+
+    PADDR:
+
+    31------------------------12 11----6 5---0
+            address(20)                D A
+ */
+/*
+ - set Accessed:
+   0001 0000  -->16
+ - set Dirty(already Accessed):
+   0011 0000  -->48
+
  */
 
-paddr_t page_translate(vaddr_t vaddr){
+paddr_t page_translate(vaddr_t vaddr,bool write){
   // level 1
   paddr_t PDE_addr = cpu.cr3 + (vaddr>> VADDR_DIR_OFFSET);
-  paddr_t PTE_addr_base = (paddr_t)paddr_read(PDE_addr,PTE_LEN);
+  paddr_t PTE_addr_base = (paddr_t)(paddr_read(PDE_addr,PTE_LEN)>>12);
+  paddr_write(PDE_addr,1,16);//set Accessed
   //level 2
   paddr_t PTE_addr = PTE_addr_base + ((vaddr<<10)>>(32-10));
-  paddr_t PADDR_addr_base = (paddr_t)paddr_read(PTE_addr,PTE_LEN);
+  paddr_t PADDR_addr_base = (paddr_t)(paddr_read(PTE_addr,PTE_LEN)>>12);
+  uint8_t DA_bits = write?48:16;
+  paddr_write(PTE_addr,1,DA_bits);//set Accessed/Dirty
   //to paddr
   return (PADDR_addr_base+((vaddr<<VADDR_OFFSET_OFFSET)>>VADDR_OFFSET_OFFSET));
 }
@@ -58,14 +74,14 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
         // int first_len = PAGE_SIZE-(addr%PAGE_SIZE);
         // int second_len = len-fist_len;
         // uint32_t readdata=0;
-        // paddr_t paddr1 = page_translate(addr);
+        // paddr_t paddr1 = page_translate(addr,false);
         // readdata = (paddr_read(paddr1,first_len)<<second_len);
-        // paddr_t paddr2 = page_translate(addr+first_len);
+        // paddr_t paddr2 = page_translate(addr+first_len,false);
         // readdata |= paddr_read(paddr2,second_len);
         // return readdata;
   }//data in one page
   else {
-        paddr_t paddr = page_translate(addr);
+        paddr_t paddr = page_translate(addr,false);
         return paddr_read(paddr, len);
   }
 }
@@ -77,13 +93,13 @@ void vaddr_write(vaddr_t addr, int len, uint32_t data) {
         // int second_len = len-first_len;
         // uint32_t data1 = data;
         // uint32_t data2 = data<<first_len;
-        // paddr_t paddr1 = page_translate(addr1);
+        // paddr_t paddr1 = page_translate(addr1,true);
         // paddr_write(paddr1,first_len,data1);
-        // paddr_t paddr2 = page_translate(addr1+first_len);
+        // paddr_t paddr2 = page_translate(addr1+first_len,true);
         // paddr_write(paddr2,second_len,data2);
         // return ;
   }else {
-        paddr_t paddr = page_translate(addr);
+        paddr_t paddr = page_translate(addr,true);
         paddr_write(paddr, len, data);
         return ;
   }
