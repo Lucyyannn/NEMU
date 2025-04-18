40a3534aa5ff04c20bcf650df01baf0110457b47
tracer-ics2017 <tracer@njuics.org>
2025-04-18 15:51:38 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:51:38 up  2:41,  1 user,  load average: 0.44, 0.36, 0.29 62a79d8c36310bce419e25e24290e7409dbd0d11

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -109,12 +109,11 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
 // xiaoduan 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
   if ((addr/PAGE_SIZE)!=((addr+len)/PAGE_SIZE)) {
-    Log("(addr/PAGE_SIZE): %d , ((addr+len)/PAGE_SIZE): %d",(addr/PAGE_SIZE),((addr+len)/PAGE_SIZE));
         int first_len = PAGE_SIZE-(addr%PAGE_SIZE);
         int second_len = len-first_len;
         uint32_t data1 = data;
         uint32_t data2 = data>>first_len;
-        Log("data:%08X , data1:%08X , data2:%08X , first_len:%d ",data,data1,data2,len);
+        Log("data:%08X , data1:%08X , data2:%08X , first_len:%d ",data,data1,data2,first_len);
         paddr_t paddr1 = page_translate(addr,true);
         paddr_write(paddr1,first_len,data1);
         paddr_t paddr2 = page_translate(addr+first_len,true);
