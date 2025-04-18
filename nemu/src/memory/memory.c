6f2bada9facf8c8c95ecea51c10e81fbe569374e
tracer-ics2017 <tracer@njuics.org>
2025-04-18 15:33:29 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:33:29 up  2:23,  1 user,  load average: 0.63, 0.33, 0.31 e81870af6ed2edfca96870cd176b316d46e12e

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -47,18 +47,18 @@ paddr_t page_translate(vaddr_t vaddr,bool write){
   }
   /* V mode */
   // level 1
-  Log("[in page_translate] cr3: %08X, vaddr:%08X",cpu.cr3,vaddr);
+  //Log("[in page_translate] cr3: %08X, vaddr:%08X",cpu.cr3,vaddr);
   paddr_t PDE_addr = (paddr_t)(cpu.cr3 + PTE_LEN*PDX(vaddr));
   uint32_t PDE_read = paddr_read(PDE_addr,PTE_LEN);
-  Log("[in page_translate] PDE_addr: %08X, PDE_read:%08X",PDE_addr,PDE_read);
-  assert(PDE_read&PTE_P);// assert the PTE exists
+  //Log("[in page_translate] PDE_addr: %08X, PDE_read:%08X",PDE_addr,PDE_read);
+  //assert(PDE_read&PTE_P);// assert the PTE exists
 
   paddr_write(PDE_addr,PTE_LEN,(PDE_read|PTE_A));//set Accessed
 
   //level 2
   paddr_t PTE_addr = PTE_ADDR(PDE_read) + PTE_LEN*PTX(vaddr);
   uint32_t PTE_read = paddr_read(PTE_addr,PTE_LEN);
-  assert(PTE_read&PTE_P);
+  //assert(PTE_read&PTE_P);
 
   uint8_t DA_bits = write?(PTE_A|PTE_D):(PTE_A);
   paddr_write(PTE_addr,PTE_LEN,(PTE_read|DA_bits));//set Accessed/Dirty
