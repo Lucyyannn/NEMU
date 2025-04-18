ab57d867b27376917ae40c9e1eb02b01058b0094
tracer-ics2017 <tracer@njuics.org>
2025-04-18 19:21:56 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:21:56 up 21 min,  1 user,  load average: 0.28, 0.30, 0.23 b99e65ae7ca8c2d42477dde9918b98472861ee8

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -17,10 +17,10 @@ make_EHelper(mov_cr){
 make_EHelper(mov_rc){
   Log("id_dest->reg: %d, id_src->reg: %d",id_dest->reg,id_src->reg);
   if(id_src->reg==0){
-    cpu.cr0=id_dest->val;
+    cpu.cr0=id_src->val;
   }else if(id_src->reg==3){
-    //Log("the value to set cr3: %08X ",id_src->val);
-    cpu.cr3=id_dest->val;
+    Log("the value to set cr3: %08X ",id_src->val);
+    cpu.cr3=id_src->val;
   }
   print_asm_template2(mov);
 }
