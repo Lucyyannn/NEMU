245cef552c190374c6dd055313f96a492dcec643
tracer-ics2017 <tracer@njuics.org>
2025-04-19 22:31:59 +0800

    >  run 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:31:59 up  1:20,  1 user,  load average: 0.31, 0.29, 0.31 3477bc4d987c9c51cee7bf787a85c00e209c06b4

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -15,31 +15,6 @@ void free_page(void *p) {
   panic("not implement yet");
 }
 
-/* The brk() system call handler. */
-int mm_brk(uint32_t new_brk) {
-  if (current->cur_brk == 0) {
-    current->cur_brk = current->max_brk = new_brk;
-  }
-  else {
-    if (new_brk > current->max_brk) {
-      // map memory region [current->max_brk, new_brk)into address space current->as
-     // void *va_begin = (void *)((current->max_brk-1)&~0xfff) + PAGE_SIZE;
-     // void *va_end = (void *)((new_brk-1)&~0xfff);
-      uintptr_t va_begin = PGROUNDDOWN(current->max_brk);
-      uintptr_t va_end   = PGROUNDDOWN(new_brk-1);
-      uintptr_t va=va_begin;
-      for(va = va_begin; va <= va_end; va += PAGE_SIZE){
-        void* pa = new_page();
-        _map(&current->as, (void*)va, pa);
-      }
-      current->max_brk = new_brk;
-    }
-    current->cur_brk = new_brk;
-  }
-
-  return 0;
-}
-
 // /* The brk() system call handler. */
 // int mm_brk(uint32_t new_brk) {
 //   if (current->cur_brk == 0) {
@@ -47,26 +22,52 @@ int mm_brk(uint32_t new_brk) {
 //   }
 //   else {
 //     if (new_brk > current->max_brk) {
-//       // TODO: map memory region [current->max_brk, new_brk)
-//       // into address space current->as
-//       uint32_t align_left = PGROUNDDOWN(current->max_brk);
-//       uint32_t align_right = PGROUNDDOWN(new_brk-1);
-//       //Log("[%x,%x)--[%x,%x)",current->max_brk,new_brk,align_left,align_right);
-//       void* cur_va = (void*)align_left;
-//       void* cur_pa = NULL;
-//       for(;align_left<=align_right;align_left+=4096){
-//         int c=0;
-//         cur_pa = _map(&current->as,cur_va,&c);
-//         assert(cur_pa);
-//         cur_va += 4096;
+//       // map memory region [current->max_brk, new_brk)into address space current->as
+//      // void *va_begin = (void *)((current->max_brk-1)&~0xfff) + PAGE_SIZE;
+//      // void *va_end = (void *)((new_brk-1)&~0xfff);
+//       uintptr_t va_begin = PGROUNDDOWN(current->max_brk);
+//       uintptr_t va_end   = PGROUNDDOWN(new_brk-1);
+//       uintptr_t va=va_begin;
+//       for(va = va_begin; va <= va_end; va += PAGE_SIZE){
+//         void* pa = new_page();
+//         _map(&current->as, (void*)va, pa);
 //       }
 //       current->max_brk = new_brk;
 //     }
-
 //     current->cur_brk = new_brk;
 //   }
+
 //   return 0;
 // }
+
+/* The brk() system call handler. */
+int mm_brk(uint32_t new_brk) {
+  if (current->cur_brk == 0) {
+    current->cur_brk = current->max_brk = new_brk;
+  }
+  else {
+    if (new_brk > current->max_brk) {
+      // TODO: map memory region [current->max_brk, new_brk)
+      // into address space current->as
+      uint32_t align_left = PGROUNDDOWN(current->max_brk);
+      uint32_t align_right = PGROUNDDOWN(new_brk-1);
+      //Log("[%x,%x)--[%x,%x)",current->max_brk,new_brk,align_left,align_right);
+      void* cur_va = (void*)align_left;
+      for(;align_left<=align_right;align_left+=4096){
+        // int c=0;
+        // cur_pa = _map(&current->as,cur_va,&c);
+        // assert(cur_pa);
+        void* pa = new_page();
+        _map(&current->as,cur_va,pa);
+        cur_va += 4096;
+      }
+      current->max_brk = new_brk;
+    }
+
+    current->cur_brk = new_brk;
+  }
+  return 0;
+}
 
 void init_mm() {
   pf = (void *)PGROUNDUP((uintptr_t)_heap.start);
