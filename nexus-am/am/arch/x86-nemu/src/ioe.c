62625a83987084d424b62cbbc3d595f48c1cd76b
tracer-ics2017 <tracer@njuics.org>
2025-04-19 17:45:38 +0800

    >  run 2213559 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:45:38 up 46 min,  1 user,  load average: 0.19, 0.55, 0.63 f0a26633f67ac9e7311de8d8fe6fe173ade646

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -1,23 +1,17 @@
 #include <am.h>
 #include <x86.h>
-#include <stdio.h>
-
 #define RTC_PORT 0x48   // Note that this is not standard
 #define I8042_DATA_PORT 0x60
 #define I8042_STATUS_PORT 0x64
-
-
 static unsigned long boot_time;
 
-// inb inl  outb outl
 void _ioe_init() {
   boot_time = inl(RTC_PORT);
 }
 
 unsigned long _uptime() {
-  unsigned long curr_time = inl(RTC_PORT);
-
-  return (curr_time-boot_time);
+  unsigned long now_time = inl(RTC_PORT);
+  return now_time-boot_time;
 }
 
 uint32_t* const fb = (uint32_t *)0x40000;
@@ -27,23 +21,36 @@ _Screen _screen = {
   .height = 300,
 };
 
+int get_width(){
+  return _screen.width;
+}
+
+int get_height(){
+  return _screen.height;
+}
+
 extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-  int dy;
-  for(dy = 0; dy < h; dy++){
-    memcpy(fb+x+(y+dy)*_screen.width , pixels + dy*w, sizeof(int)*w);
+  
+  int i,t;
+  i=y*_screen.width+x;
+  for (t=1;t<=h;t++) {
+    for(int j=0; j<w;j++){
+      fb[i+j]=*pixels;
+    }
+    i+=_screen.width;
   }
-  return;
 }
 
 void _draw_sync() {
-  
 }
 
 int _read_key() {
-  if(inb(I8042_STATUS_PORT)==1){
-    return inl(I8042_DATA_PORT);
+  int key=0;
+  int flag = inb(I8042_STATUS_PORT);
+  if(flag){
+    key = inl(I8042_DATA_PORT);
   }
-  return _KEY_NONE;
+  return key;
 }
