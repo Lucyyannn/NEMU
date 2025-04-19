a8be9a331e83a80224034a3a7c3e27e8f664a556
tracer-ics2017 <tracer@njuics.org>
2025-04-19 17:40:50 +0800

    >  run 2213559 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:40:50 up 42 min,  1 user,  load average: 0.73, 0.70, 0.68 17ce761b33d2f6d5fb86815e6a54d2728c154634

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -88,6 +88,51 @@ void _switch(_Protect *p) {
 //   }
 //   return ;
 // }
+
+
+// void _unmap(_Protect *p, void *va) {
+// }
+
+// _RegSet *_umake(_Protect *p, _Area ustack, _Area kstack, 
+//               void *entry, char *const argv[], char *const envp[]) {
+//                 /*
+//                  stack frame : high address
+//                                               return value
+//                                               argv n
+//                                               argv n-1
+//                                               ...
+//                                               argv 1
+//                                low            old ebp
+//                  */
+//   //(1) set the stack of _start
+//   uintptr_t StartStack = (uintptr_t)ustack.end;
+//   *(uintptr_t*)StartStack=0; //return value
+//   *((char**)(--StartStack)) = NULL;//arguments
+//   *((char**)(--StartStack))=NULL;
+//   *((int*)(--StartStack))=0;
+  
+//   //(2) init trapframe
+//   //_RegSet* tf = (_RegSet*)(StartStack - TF_SPACE/sizeof(int));
+//   //_RegSet* tf = (_RegSet*)(ustack.end-sizeof(uintptr_t)*4-TF_SPACE);
+//   _RegSet* tf = (_RegSet*)(StartStack-TF_SPACE);
+//   tf->edi=0;
+//   tf->esi=0;
+//   tf->ebp=0;
+//   tf->esp=0;
+//   tf->ebx=0;
+//   tf->edx=0;
+//   tf->ecx=0;
+//   tf->eax=0;
+//   tf->irq=0;
+//   tf->error_code=0;
+//   tf->eip=(uintptr_t)entry;
+//   tf->cs=8;
+//   tf->eflags=0x2;
+
+//   return tf;
+// }
+
+
 void* _map(_Protect *p, void *va,int* cnt) {
   PDE *dir = (PDE*)(p->ptr);//get the PDE 
   dir += PDX(va);
@@ -116,42 +161,14 @@ void* _map(_Protect *p, void *va,int* cnt) {
 void _unmap(_Protect *p, void *va) {
 }
 
-_RegSet *_umake(_Protect *p, _Area ustack, _Area kstack, 
-              void *entry, char *const argv[], char *const envp[]) {
-                /*
-                 stack frame : high address
-                                              return value
-                                              argv n
-                                              argv n-1
-                                              ...
-                                              argv 1
-                               low            old ebp
-                 */
-  //(1) set the stack of _start
-  uintptr_t StartStack = (uintptr_t)ustack.end;
-  *(uintptr_t*)StartStack=0; //return value
-  *((char**)(--StartStack)) = NULL;//arguments
-  *((char**)(--StartStack))=NULL;
-  *((int*)(--StartStack))=0;
-  
-  //(2) init trapframe
-  //_RegSet* tf = (_RegSet*)(StartStack - TF_SPACE/sizeof(int));
-  //_RegSet* tf = (_RegSet*)(ustack.end-sizeof(uintptr_t)*4-TF_SPACE);
-  _RegSet* tf = (_RegSet*)(StartStack-TF_SPACE);
-  tf->edi=0;
-  tf->esi=0;
-  tf->ebp=0;
-  tf->esp=0;
-  tf->ebx=0;
-  tf->edx=0;
-  tf->ecx=0;
-  tf->eax=0;
-  tf->irq=0;
-  tf->error_code=0;
-  tf->eip=(uintptr_t)entry;
-  tf->cs=8;
-  tf->eflags=0x2;
-
-  return tf;
+_RegSet *_umake(_Protect *p, _Area ustack, _Area kstack, void *entry, char *const argv[], char *const envp[]) {
+  *((char**)(ustack.end -4))=NULL;
+  *((char**)(ustack.end -8))=NULL;
+  *((int*)(ustack.end -12))=0;
+  _RegSet* rs = (_RegSet*)(ustack.end - 12 - sizeof(_RegSet));
+  rs->cs = 0x8;
+  rs->eip = (uintptr_t)entry;
+  rs->eflags = (1<<9)+2;
+  return rs;
 }
 
