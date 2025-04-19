fbd5bca6faa4277eca0de5bae66db32fcc3035c2
tracer-ics2017 <tracer@njuics.org>
2025-04-19 18:32:54 +0800

    >  compile 2213559 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:32:54 up  1:34,  1 user,  load average: 0.65, 0.71, 0.72 69b01bc43ef9b18a4f07d754a6521f3ac32367

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,21 @@
+
 #include "nemu.h"
+#include "device/mmio.h"
+
 #define PMEM_SIZE (128 * 1024 * 1024)
+#define PAGE_SIZE 4096
+#define PTE_LEN 4
+#define PGSHFT    12      // log2(PGSIZE)
+#define PTXSHFT   12      // Offset of PTX in a linear address
+#define PDXSHFT   22      // Offset of PDX in a linear address
+#define PDX(va)     (((uint32_t)(va) >> PDXSHFT) & 0x3ff)
+#define PTX(va)     (((uint32_t)(va) >> PTXSHFT) & 0x3ff)
+#define OFF(va)     ((uint32_t)(va) & 0xfff)
+#define PTE_ADDR(pte)   ((uint32_t)(pte) & ~0xfff)
+#define PTE_P     0x001     // Present
+#define PTE_A     0x020     // Accessed
+#define PTE_D     0x040     // Dirty
+
 
 #define pmem_rw(addr, type) *(type *)({\
     Assert(addr < PMEM_SIZE, "physical address(0x%08x) is out of bound", addr); \
@@ -7,111 +23,114 @@
     })
 
 uint8_t pmem[PMEM_SIZE];
+/*  VADDR:
+
+   31----------22 21----------12 11----------0
+      DIR(10)         PAGE(10)      OFFSET(12)
+
+    PADDR:
+
+    31------------------------12 11----6 5---0
+            address(20)                D A   P
+ */
+/*
+ - set Accessed:
+   0001 0000  -->16
+ - set Dirty(already Accessed):
+   0011 0000  -->48
+
+ */
+
+paddr_t page_translate(vaddr_t vaddr,bool write){
+  /*  P mode */
+  if (!(cpu.cr0>>31)){
+    return (paddr_t)vaddr;
+  }
+  /* V mode */
+  // level 1
+  //Log("[in page_translate] cr3: %08X, vaddr:%08X",cpu.cr3,vaddr);
+  paddr_t PDE_addr = (paddr_t)(cpu.cr3 + PTE_LEN*PDX(vaddr));
+  uint32_t PDE_read = paddr_read(PDE_addr,PTE_LEN);
+  //Log("[in page_translate] PDE_addr: %08X, PDE_read:%08X",PDE_addr,PDE_read);
+  assert(PDE_read&PTE_P);// assert the PTE exists
+
+  paddr_write(PDE_addr,PTE_LEN,(PDE_read|PTE_A));//set Accessed
+
+  //level 2
+  paddr_t PTE_addr = PTE_ADDR(PDE_read) + PTE_LEN*PTX(vaddr);
+  uint32_t PTE_read = paddr_read(PTE_addr,PTE_LEN);
+  //Log("[in page_translate] PTE_addr: %08X, PTE_read:%08X",PTE_addr,PTE_read);
+  assert(PTE_read&PTE_P);
+
+  uint8_t DA_bits = write?(PTE_A|PTE_D):(PTE_A);
+  paddr_write(PTE_addr,PTE_LEN,(PTE_read|DA_bits));//set Accessed/Dirty
+  
+  //to paddr
+  return (paddr_t)(PTE_ADDR(PTE_read)+OFF(vaddr));
+}
 
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
- int flag = is_mmio(addr);
- if(flag==-1){
-   return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
- }
-  return mmio_read(addr,len,flag);
+  int num = is_mmio(addr);
+  if(num!=-1){
+    return mmio_read(addr,len,num);
+  }else{
+    return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  }
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  int flag = is_mmio(addr);
-  if(flag==-1){
+  int num = is_mmio(addr);
+  if(num!=-1){
+    mmio_write(addr,len,data,num);
+    return;
+  }else{
     memcpy(guest_to_host(addr), &data, len);
-  }else{
-    mmio_write(addr,len,data,flag);
-  }
-}
-//static int cnt =0;
-paddr_t page_translate(vaddr_t va,int write){
-  if(cpu.cr0>>31){
-    paddr_t dir_base = PTE_ADDR(cpu.cr3);
-    
-    uint32_t dir_idx = PDX(va);
-    paddr_t pde_addr = dir_base + dir_idx * 4;
-    uint32_t pde = paddr_read(pde_addr, 4);
-    assert(pde&PTE_P);
-    if(!(pde&PTE_A)){
-      pde |= PTE_A;
-      paddr_write(pde_addr, 4, pde);
-    }
-
-    paddr_t pt_base = PTE_ADDR(pde);
-    uint32_t pt_idx = PTX(va);
-    paddr_t pte_addr = pt_base + pt_idx*4;
-    uint32_t pte = paddr_read(pte_addr, 4);
-    // if(!(pte&PTE_P)){
-    //   printf("va:%x,w/r:%x\n",va,write);
-    //   printf("dir_idx:%x,pde:%x\n",dir_idx,pde);
-    //   printf("pt_base:%x,pt_idx:%x,pte:%x\n",pt_base,pt_idx,pte);
-    // }
-    assert(pte&PTE_P);
-
-    if(!(pte&PTE_A)){
-      pte |= PTE_A;
-      paddr_write(pte_addr, 4, pte);
-    }
-    if(write&&!(pte&PTE_D)){
-      pte |= PTE_D;
-      paddr_write(pte_addr, 4, pte);
-    }
-
-    paddr_t page_base = PTE_ADDR(pte);
-    uint32_t page_off = OFF(va);
-    paddr_t data_addr = page_base + page_off;
-    // if(cnt++<10){
-    //   printf("The va:%x|pd:%x|pde:%x|pt:%x|pte:%x|pa:%x\n",va,dir_base,pde,pt_base,pte,data_addr);
-    // }
-    return data_addr;
-  }else{
-    return va;
+    return ;
   }
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  if(len > 4){
-    len = 4;
-  }
-  if(0){
-    assert(0);
-    return 0;
-  }else{
-    paddr_t paddr = page_translate(addr,0);
-    int r_len = 4096-(OFF(paddr));
-    if(r_len < len){
-      vaddr_t la_va = PGROUNDUP(addr);
-      paddr_t la_pa = page_translate(la_va,0);
-      uint32_t lo_data = paddr_read(paddr, r_len);
-      uint32_t hi_data = paddr_read(la_pa, len - r_len);
-      uint32_t data = (hi_data << (r_len*8))+ lo_data;
-      return data;
-    }else{
-      return paddr_read(paddr, len);
-    }
+   int rest_size = 4096 - addr%4096;
+  //data cross page
+  if (rest_size<len) {
+        int first_len = rest_size;
+        int second_len = len-first_len;
+        assert(first_len>0&&second_len>0);
+
+        uint32_t readdata=0;
+        paddr_t paddr1 = page_translate(addr,false);
+        readdata = paddr_read(paddr1,first_len);//lower bits
+        paddr_t paddr2 = page_translate(addr+first_len,false);
+        readdata |= (paddr_read(paddr2,second_len)<<(first_len*8));//higher bits
+        return readdata;
+  }//data in one page
+  else {
+        paddr_t paddr = page_translate(addr,false);
+        return paddr_read(paddr, len);
   }
 }
+// xiaoduan 
+
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  if(len > 4){
-    len = 4;
-  }
-  if(0){
-    assert(0);
-  }else{
-    paddr_t paddr = page_translate(addr,1);
-    int r_len = 4096-(OFF(paddr));
-    if(r_len < len){
-      vaddr_t la_va = PGROUNDUP(addr);
-      paddr_t la_pa = page_translate(la_va,1);
-      paddr_write(paddr, r_len, data);
-      uint32_t hi_data = data >> (r_len*8);
-      paddr_write(la_pa, len - r_len, hi_data);
-    }else{
-      paddr_write(paddr, len, data);
-    }
+  int rest_size = 4096 - addr%4096;
+  if (rest_size<len) {
+        int first_len = rest_size;
+        int second_len = len-first_len;
+        assert(second_len>0&&first_len>0);
+
+        uint32_t data1 = (data<<(second_len*8))>>(second_len*8);
+        uint32_t data2 = data>>(first_len*8);
+        paddr_t paddr1 = page_translate(addr,true);
+        paddr_write(paddr1,first_len,data1);
+        paddr_t paddr2 = page_translate(addr+first_len,true);
+        paddr_write(paddr2,second_len,data2);
+        return ;
+  }else {
+        paddr_t paddr = page_translate(addr,true);
+        paddr_write(paddr, len, data);
+        return ;
   }
 }
