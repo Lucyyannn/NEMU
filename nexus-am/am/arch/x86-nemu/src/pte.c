4e18d59101a49bfc6d42bc0f6bde2f7e3b2501d9
tracer-ics2017 <tracer@njuics.org>
2025-04-19 22:29:15 +0800

    >  run 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:29:15 up  1:18,  1 user,  load average: 0.14, 0.20, 0.29 3ef4c1fb8fb0dba83ba92f8339d0f2f2dea9190d

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -69,25 +69,25 @@ void _switch(_Protect *p) {
   set_cr3(p->ptr);
 }
 
-// void _map(_Protect *p, void *va, void *pa) {
-//   //the value of va and pa is already difinited. What I should do is to set the PDE and PTE to support the bridge
-//   PDE* pde = (PDE*)p->ptr + PDX(va);
-//   // (1)if PTE not exists
-//   if(!(*pde&PTE_P)){
-//     PTE* ptepage = (PTE*)palloc_f();//alloc a page for PTE
-//     *pde = PTE_ADDR(*ptepage)| PTE_P;// set the PTE value in PDE
+void _map(_Protect *p, void *va, void *pa) {
+  //the value of va and pa is already difinited. What I should do is to set the PDE and PTE to support the bridge
+  PDE* pde = (PDE*)p->ptr + PDX(va);
+  // (1)if PTE not exists
+  if(!(*pde&PTE_P)){
+    PTE* ptepage = (PTE*)palloc_f();//alloc a page for PTE
+    *pde = PTE_ADDR(*ptepage)| PTE_P;// set the PTE value in PDE
 
-//     PTE* pte = (PTE*)(PTE_ADDR(*pde)+ PTE_LEN*PTX(va));
-//     *pte = PTE_ADDR(pa) | PTE_P; //update PTE
-//     return ;
+    PTE* pte = (PTE*)(PTE_ADDR(*pde)+ PTE_LEN*PTX(va));
+    *pte = PTE_ADDR(pa) | PTE_P; //update PTE
+    return ;
 
-//   }else{//(2)PTE exists
-//     PTE* pte = (PTE*)(PTE_ADDR(*pde)+PTE_LEN*PTX(va));
-//     *pte = PTE_ADDR(pa) | PTE_P; //update PTE
-//     return ;
-//   }
-//   return ;
-// }
+  }else{//(2)PTE exists
+    PTE* pte = (PTE*)(PTE_ADDR(*pde)+PTE_LEN*PTX(va));
+    *pte = PTE_ADDR(pa) | PTE_P; //update PTE
+    return ;
+  }
+  return ;
+}
 
 
 void _unmap(_Protect *p, void *va) {
@@ -134,37 +134,27 @@ _RegSet *_umake(_Protect *p, _Area ustack, _Area kstack,
 
 
 
-void* _map(_Protect *p, void *va,int* cnt) {
-  PDE *dir = (PDE*)(p->ptr);//get the PDE 
-  dir += PDX(va);
-  PTE* uppte=NULL; 
-  if(*dir&PTE_P){ 
-    uppte = (PTE*)PTE_ADDR(*dir);
-  }else{//not exist!
-    uppte = (PTE*)(palloc_f());
-    *dir = ((uint32_t)uppte&(~0xfff))|PTE_P;
-    *cnt+=1;
-  }
-  uppte += PTX(va);
-  void* pa=NULL;
-  if(*uppte&PTE_P){
-    pa = (void*)PTE_ADDR(*uppte);
-  }else{//not exist!
-    pa = (void*)(palloc_f());
-    *uppte = ((uint32_t)pa&(~0xfff))|PTE_P;
-    *cnt+=1;
-  }
-  pa += OFF(va);
-  return pa;
-}
-
-// _RegSet *_umake(_Protect *p, _Area ustack, _Area kstack, void *entry, char *const argv[], char *const envp[]) {
-//   *((char**)(ustack.end -4))=NULL;
-//   *((char**)(ustack.end -8))=NULL;
-//   *((int*)(ustack.end -12))=0;
-//   _RegSet* rs = (_RegSet*)(ustack.end - 12 - sizeof(_RegSet));
-//   rs->cs = 0x8;
-//   rs->eip = (uintptr_t)entry;
-//   rs->eflags = (1<<9)+2;
-//   return rs;
+// void* _map(_Protect *p, void *va,int* cnt) {
+//   PDE *dir = (PDE*)(p->ptr);//get the PDE 
+//   dir += PDX(va);
+//   PTE* uppte=NULL; 
+//   if(*dir&PTE_P){ 
+//     uppte = (PTE*)PTE_ADDR(*dir);
+//   }else{//not exist!
+//     uppte = (PTE*)(palloc_f());
+//     *dir = ((uint32_t)uppte&(~0xfff))|PTE_P;
+//     *cnt+=1;
+//   }
+//   uppte += PTX(va);
+//   void* pa=NULL;
+//   if(*uppte&PTE_P){
+//     pa = (void*)PTE_ADDR(*uppte);
+//   }else{//not exist!
+//     pa = (void*)(palloc_f());
+//     *uppte = ((uint32_t)pa&(~0xfff))|PTE_P;
+//     *cnt+=1;
+//   }
+//   pa += OFF(va);
+//   return pa;
 // }
+
