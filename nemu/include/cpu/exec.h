042fef4dd286e31ff336e5c30c744c21ea4a896f
tracer-ics2017 <tracer@njuics.org>
2025-04-19 17:35:04 +0800

    >  run 2213559 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:35:04 up 36 min,  1 user,  load average: 0.60, 0.72, 0.66 920f68b9892cc5b4b8c81a342429d24f44837ae1

--- nemu/include/cpu/exec.h
+++ nemu/include/cpu/exec.h
@@ -8,19 +8,14 @@ typedef void (*EHelper) (vaddr_t *);
 
 #include "cpu/decode.h"
 
-void raise_intr(uint8_t NO, vaddr_t ret_addr);
-
-// fetch a instruction
 static inline uint32_t instr_fetch(vaddr_t *eip, int len) {
   uint32_t instr = vaddr_read(*eip, len);
-
 #ifdef DEBUG
   uint8_t *p_instr = (void *)&instr;
   int i;
   for (i = 0; i < len; i ++) {
     decoding.p += sprintf(decoding.p, "%02x ", p_instr[i]);
   }
-  
 #endif
   (*eip) += len;
   return instr;
--- nemu/include/monitor/monitor.h
+++ nemu/include/monitor/monitor.h
@@ -4,7 +4,4 @@
 enum { NEMU_STOP, NEMU_RUNNING, NEMU_END };
 extern int nemu_state;
 
-char* w_tokens[32]; // from w_tokens[0] to w_tokens[31]
-int w_nr;
-
 #endif
