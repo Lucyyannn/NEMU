95837ad5851856027288a31aca725fe3867e67da
tracer-ics2017 <tracer@njuics.org>
2025-04-19 18:04:06 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:04:06 up  1:05,  1 user,  load average: 0.82, 0.55, 0.56 5d7be30b5738a3d5486a175b470b170bd14c6613

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -1,89 +1,17 @@
-// #include "cpu/exec.h"
-
-// void diff_test_skip_qemu();
-// void diff_test_skip_nemu();
-
-// make_EHelper(lidt) {
-//   if(id_dest->width==2){
-//     cpu.idtr.limit = vaddr_read(id_dest->addr,2);
-//     cpu.idtr.base  = vaddr_read(id_dest->addr+2,3);
-//   }else if(id_dest->width==4){
-//     cpu.idtr.limit = vaddr_read(id_dest->addr,2);
-//     cpu.idtr.base = vaddr_read(id_dest->addr+2,4);
-//   }
-
-//   print_asm_template1(lidt);
-// }
-
-// make_EHelper(mov_r2cr) {
-//   TODO();
-
-//   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
-// }
-
-// make_EHelper(mov_cr2r) {
-//   TODO();
-
-//   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
-
-// #ifdef DIFF_TEST
-//   diff_test_skip_qemu();
-// #endif
-// }
-
-// make_EHelper(int) {
-//   raise_intr(id_dest->val,decoding.seq_eip);
-
-//   print_asm("int %s", id_dest->str);
-
-// #ifdef DIFF_TEST
-//   diff_test_skip_nemu();
-// #endif
-// }
-
-// make_EHelper(iret) {
-//   rtl_pop(&decoding.jmp_eip);
-//   decoding.is_jmp = 1;
-//   rtl_pop(&cpu.CS);
-//   rtl_pop(&cpu.eflags);
-
-//   print_asm("iret");
-// }
-
-// uint32_t pio_read(ioaddr_t, int);
-// void pio_write(ioaddr_t, int, uint32_t);
-
-
-// make_EHelper(in) {
-//   t0 = pio_read(id_src->val,id_src->width);
-//   operand_write(id_dest,&t0);
-
-//   print_asm_template2(in);
-
-// #ifdef DIFF_TEST
-//   diff_test_skip_qemu();
-// #endif
-// }
-
-// make_EHelper(out) {
-//   pio_write(id_dest->val,id_src->width,id_src->val);
-
-//   print_asm_template2(out);
-
-// #ifdef DIFF_TEST
-//   diff_test_skip_qemu();
-// #endif
-// }
 #include "cpu/exec.h"
 
 void diff_test_skip_qemu();
 void diff_test_skip_nemu();
-void raise_intr(uint8_t NO, vaddr_t ret_addr);
 
 make_EHelper(lidt) {
-  cpu.idtr.limit = vaddr_read(id_dest->addr,2);
-  cpu.idtr.base = vaddr_read(id_dest->addr+4,2)<<16;
-  cpu.idtr.base += vaddr_read(id_dest->addr+2,2);
+  if(id_dest->width==2){
+    cpu.idtr.limit = vaddr_read(id_dest->addr,2);
+    cpu.idtr.base  = vaddr_read(id_dest->addr+2,3);
+  }else if(id_dest->width==4){
+    cpu.idtr.limit = vaddr_read(id_dest->addr,2);
+    cpu.idtr.base = vaddr_read(id_dest->addr+2,4);
+  }
+
   print_asm_template1(lidt);
 }
 
@@ -104,7 +32,8 @@ make_EHelper(mov_cr2r) {
 }
 
 make_EHelper(int) {
-  raise_intr(id_dest->imm,*eip);
+  raise_intr(id_dest->val,decoding.seq_eip);
+
   print_asm("int %s", id_dest->str);
 
 #ifdef DIFF_TEST
@@ -113,30 +42,29 @@ make_EHelper(int) {
 }
 
 make_EHelper(iret) {
-  rtl_pop(&t2);
-  decoding.jmp_eip = t2;
-  rtl_pop(&t2);
-  cpu.CS = t2;
-  rtl_pop(&t2);
-  cpu.eflags = t2;
+  rtl_pop(&decoding.jmp_eip);
   decoding.is_jmp = 1;
+  rtl_pop(&cpu.CS);
+  rtl_pop(&cpu.eflags);
 
   print_asm("iret");
 }
 
 uint32_t pio_read(ioaddr_t, int);
 void pio_write(ioaddr_t, int, uint32_t);
-//302
+
+
 make_EHelper(in) {
-  t2 = pio_read(id_src->val,id_src->width);
-  operand_write(id_dest,&t2);
+  t0 = pio_read(id_src->val,id_src->width);
+  operand_write(id_dest,&t0);
+
   print_asm_template2(in);
 
 #ifdef DIFF_TEST
   diff_test_skip_qemu();
 #endif
 }
-//358
+
 make_EHelper(out) {
   pio_write(id_dest->val,id_src->width,id_src->val);
 
