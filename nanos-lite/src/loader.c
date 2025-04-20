9fb9bcaf61f39f6cc450d319a4ae9c1ee37e7d02
tracer-ics2017 <tracer@njuics.org>
2025-04-20 18:33:23 +0800

    >  run 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:33:23 up 15 min,  1 user,  load average: 0.28, 0.41, 0.35 711335ff599c2d8e3fde11a4dfe8548d2b0521e9

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,57 +9,22 @@ ssize_t fs_read(int fd, void *buf, size_t len);
 int fs_close(int fd);
 void* new_page(void);
 void _map(_Protect *p, void *va, void *pa);
-//void* _map(_Protect *p, void *va, int *cnt);
 
-// uintptr_t loader(_Protect *as, const char *filename) {
-//   int fd = fs_open(filename,0,0);
-//   size_t filesz = fs_filesz(fd);
-//   int page_num = filesz/PAGE_SIZE; //total_pages+rest_content
-//   if(filesz%PAGE_SIZE>0){page_num+=1;}
-
-//   void* va = DEFAULT_ENTRY;//load the program by DEFAULT_ENTRY in vitual logic
-//   for(int i=0;i<page_num;i++){
-//     void* pa = new_page();
-//     fs_read(fd,pa,PAGE_SIZE);//load
-//     //Log("load file: %s , at physical addr: %08X",filename,pa);
-//     _map(as, va, pa);//record the reflection
-//     va+=PAGE_SIZE;
-//   }
-//   fs_close(fd);
-  
-//   return (uintptr_t)DEFAULT_ENTRY;
-//  }
-
-
-// uintptr_t loader(_Protect *as, const char *filename) {
-//   int fd = fs_open(filename,0,0);
-//   int file_size = fs_filesz(fd);
-//   Log("%d,%x,%x-%x",file_size,as->ptr,as->area.start,as->area.end);
-//   void* cur_va = DEFAULT_ENTRY;
-//   void* cur_pa = NULL;
-//   for(int i=0;i<file_size;i+=4096){
-//     int c=0;
-//     cur_pa = _map(as,cur_va,&c);
-//     assert(cur_pa);
-//     //Log("va:%x,pa:%x,%d",(uint32_t)cur_va,(uint32_t)cur_pa,c);
-//     fs_read(fd,cur_pa,4096);
-//     cur_va += 4096;
-//   }
-//   return (uintptr_t)DEFAULT_ENTRY;
-// }
 uintptr_t loader(_Protect *as, const char *filename) {
-  int fd = fs_open(filename, 0, 0);
-  int i, pages = fs_filesz(fd) / PGSIZE + 1;
-  void *pa, *va;
-  
-  for (i = 0, va = DEFAULT_ENTRY; i < pages; ++i, va += PGSIZE) {
-    pa = new_page();
-    // printf("pa: %x, va: %x", pa, va);
-    // printf(", read: %d\n", fs_read(fd, pa, PGSIZE));
-    fs_read(fd, pa, PGSIZE);
-    _map(as, va, pa);
-  }
+  int fd = fs_open(filename,0,0);
+  size_t filesz = fs_filesz(fd);
+  int page_num = filesz/PAGE_SIZE; //total_pages+rest_content
+  if(filesz%PAGE_SIZE>0){page_num+=1;}
 
+  void* va = DEFAULT_ENTRY;//load the program by DEFAULT_ENTRY in vitual logic
+  for(int i=0;i<page_num;i++){
+    void* pa = new_page();
+    fs_read(fd,pa,PAGE_SIZE);//load
+    //Log("load file: %s , at physical addr: %08X",filename,pa);
+    _map(as, va, pa);//record the reflection
+    va+=PAGE_SIZE;
+  }
   fs_close(fd);
+  
   return (uintptr_t)DEFAULT_ENTRY;
-}
+ }
--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -38,36 +38,6 @@ void free_page(void *p) {
 //   return 0;
 // }
 
-// void* _map(_Protect *p, void *va,int *cnt);
-// /* The brk() system call handler. */
-// int mm_brk(uint32_t new_brk) {
-//   if (current->cur_brk == 0) {
-//     current->cur_brk = current->max_brk = new_brk;
-//   }
-//   else {
-//     if (new_brk > current->max_brk) {
-//       // TODO: map memory region [current->max_brk, new_brk)
-//       // into address space current->as
-//       uint32_t align_left = PGROUNDDOWN(current->max_brk);
-//       uint32_t align_right = PGROUNDDOWN(new_brk-1);
-//       //Log("[%x,%x)--[%x,%x)",current->max_brk,new_brk,align_left,align_right);
-//       void* cur_va = (void*)align_left;
-//       void* cur_pa = NULL;
-//       for(;align_left<=align_right;align_left+=4096){
-//         int c=0;
-//         cur_pa = _map(&current->as,cur_va,&c);
-//         assert(cur_pa);
-//         cur_va += 4096;
-//       }
-//       current->max_brk = new_brk;
-//     }
-
-//     current->cur_brk = new_brk;
-//   }
-//   return 0;
-// }
-
-
 int mm_brk(uint32_t new_brk) {
   //printf("cur_brk: %x, max_brk: %x, new_brk: %x\n", current->cur_brk, current->max_brk, new_brk);
   if (current->cur_brk == 0)
