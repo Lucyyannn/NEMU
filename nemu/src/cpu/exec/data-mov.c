9b80478e5b6782e083c84f1eeeae91a91887f022
tracer-ics2017 <tracer@njuics.org>
2025-04-20 21:50:06 +0800

    >  compile 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:50:06 up  3:31,  1 user,  load average: 0.43, 0.44, 0.50 ed2c067e1c98f5751afca7f82d543021459dd882

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -103,15 +103,16 @@ make_EHelper(cltd) {
 }
 
 make_EHelper(cwtl) {
+  // AX <-- AL(sext)
   if (decoding.is_operand_size_16) {
-    TODO();
+    rtl_lr(&t0,0,1);
+    rtl_sext(&t1,&t0,1);
+    rtl_sr(0,2,&t1);
   }
-  else {
-    if((int)reg_w(0)<0){
-      cpu.gpr[2]._16 = 0xFFFF;
-    }else{
-      cpu.gpr[2]._16 = 0;
-    }
+  else {//EAX <-- AX (sext)
+    rtl_lr(&t0,0,2);
+    rtl_sext(&t1,&t0,2);
+    rtl_sr(0,4,&t1);
   }
 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
