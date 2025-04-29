f98e7dd91920af67f12ac1890a172481098a0be3
tracer-ics2017 <tracer@njuics.org>
2025-04-29 16:34:40 +0800

    >  run 2213583 lucy Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:34:40 up  3:10,  1 user,  load average: 0.42, 0.38, 0.27 d9d842b99b5461f12fe39bf5f7a3562f3fa1bdb5

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -25,7 +25,7 @@ int mm_brk(uint32_t new_brk) {
       // map memory region [current->max_brk, new_brk)into address space current->as
       uintptr_t va_begin = PGROUNDUP(current->max_brk);
       uintptr_t va_end = PGROUNDDOWN(new_brk-1);
-      Log("[mm_brk] max_brk: %08X, new_brk: %08X ,va_begin=%08X,va_end=%08X",current->max_brk,new_brk,va_begin,va_end);
+      //Log("[mm_brk] max_brk: %08X, new_brk: %08X ,va_begin=%08X,va_end=%08X",current->max_brk,new_brk,va_begin,va_end);
       for(uintptr_t va = va_begin; va <= va_end; va += PAGE_SIZE){
         void* pa = new_page();
         _map(&current->as, (void*)va, pa);
